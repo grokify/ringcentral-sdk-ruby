16af0cedf9547485494d92453ce7bfe9a9cbd3b2
John Wang <jwang@proofpoint.com>
2015-05-18 23:05:34 -0700

    add convenience method

--- lib/ringcentral_sdk/sdk.rb
+++ lib/ringcentral_sdk/sdk.rb
@@ -7,9 +7,16 @@ module RingCentralSdk
     attr_reader :parser
     attr_reader :platform
 
-    def initialize(app_key=nil,app_secret=nil,server_url=nil)
+    def initialize(app_key=nil,app_secret=nil,server_url=nil,username=nil,extension=nil,password=nil)
       @parser   = RingCentralSdk::Platform::Parser.new
       @platform = RingCentralSdk::Platform::Platform.new(app_key, app_secret, server_url)
+      if not username.nil? and not password.nil?
+        @platform.authorize(username, extension, password)
+      end
+    end
+
+    def request(helper=nil)
+      return @platform.request(helper)
     end
   end
 end
