a83227106b67760ec97874382c85ccf7101b608f
John Wang <johncwang@gmail.com>
2015-10-09 14:36:17 -0700

    update tests

--- test/test_pubnub.rb
+++ test/test_pubnub.rb
@@ -4,14 +4,24 @@ class RingCentralSdkPubnubTest < Test::Unit::TestCase
   def testSetup
 
     rcsdk = RingCentralSdk::Sdk.new(
-      "myAppKey",
-      "myAppSecret",
+      'myAppKey',
+      'myAppSecret',
       RingCentralSdk::Sdk::RC_SERVER_SANDBOX
     )
 
-    sub = rcsdk.create_subscription()
+    pubnub_factory = RingCentralSdk::PubnubFactory.new()
+    assert_equal 'RingCentralSdk::PubnubFactory', pubnub_factory.class.name
 
+    pubnub = pubnub_factory.pubnub('', '', nil)
+    assert_equal 'Pubnub::Client', pubnub.class.name
+
+    sub = rcsdk.create_subscription()
     assert_equal "RingCentralSdk::Subscription", sub.class.name
+
+    pubnub_factory2 = RingCentralSdk::PubnubFactory.new(true)
+    assert_raise do
+      pubnub_factory2.pubnub('', '', nil)
+    end
 
   end
 end
