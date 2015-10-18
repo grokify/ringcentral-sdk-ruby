d1f6d52c1459864b313de5bff589161497216311
John Wang <johncwang@gmail.com>
2015-10-18 02:17:54 -0700

    update ringcentral_sdk.rb

--- lib/ringcentral_sdk.rb
+++ lib/ringcentral_sdk.rb
@@ -1,4 +1,5 @@
 module RingCentralSdk
+
   VERSION = '0.5.0'
 
   RC_SERVER_PRODUCTION = 'https://platform.ringcentral.com'
@@ -6,14 +7,12 @@ module RingCentralSdk
 
   autoload :Helpers, 'ringcentral_sdk/helpers'
   autoload :Platform, 'ringcentral_sdk/platform'
-  autoload :Sdk, 'ringcentral_sdk/sdk'
   autoload :Subscription, 'ringcentral_sdk/subscription'
 
   class << self
-
     def new(app_key=nil, app_secret=nil, server_url=nil, opts={})
       RingCentralSdk::Platform.new(app_key, app_secret, server_url, opts)
     end
-
   end
+
 end
