5accb376bbda056e7232bdb5bfbc10399d20d0a6
John Wang <johncwang@gmail.com>
2016-01-25 08:41:53 -0800

    update voicebase scripts

--- scripts/call-recording_transcribe_voicebase.rb
+++ scripts/call-recording_transcribe_voicebase.rb
@@ -5,6 +5,7 @@ require 'faraday'
 require 'faraday_middleware'
 require 'multi_json'
 require 'ringcentral_sdk'
+require 'voicebase'
 require 'pp'
 
 =begin
@@ -27,53 +28,16 @@ client = RingCentralSdk::REST::Client.new
 client.app_config(config.app)
 client.authorize_user(config.user)
 
-module VoiceBase
-  class Client
-    def initialize(api_key, password, transcript_type='machine-best')
-      @api_key = api_key
-      @password = password
-      @transcript_type = transcript_type
-      @conn = Faraday.new(:url => 'https://api.voicebase.com/services') do |faraday|
-        faraday.request  :multipart               # multipart/form-data
-        faraday.response :json
-        faraday.response :logger                  # log requests to STDOUT
-        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-      end
-    end
-
-    def upload_media(filepath, content_type)
-      params = {
-        :version        => '1.1',
-        :apikey         => @api_key,
-        :password       => @password,
-        :action         => 'uploadMedia',
-        :file           => Faraday::UploadIO.new(filepath, content_type),
-        :title          => DateTime.now.strftime('%Y-%m-%d %I:%M:%S %p'),
-        :transcriptType => @transcript_type,
-        :desc           => 'file description',
-        :recordedDate   => DateTime.now.strftime('%Y-%m-%d %I:%M:%S'),
-        :collection     => '',
-        :public         => false,
-        :sourceUrl      => '',
-        :lang           => 'en',
-        :imageUrl       => ''
-      }
-      response = @conn.post '/services', params
-      pp response
-      puts response.body['fileUrl']
-    end
-  end
-end
-
 def upload_recordings(vbsdk, dir)
   Dir.glob('recording_*.mp3').each_with_index do |file,i|
     puts file
-    vbsdk.upload_media(file, 'audio/mpeg')
+    res = vbsdk.upload_media(:filePath => file, :fileContentType => 'audio/mpeg', :desc => 'myfile')
+    pp res.body
     break
   end
 end
 
-vbsdk = VoiceBase::Client.new(
+vbsdk = VoiceBase::V1::Client.new(
   config.env.data['RC_DEMO_VB_API_KEY'],
   config.env.data['RC_DEMO_VB_PASSWORD'])
 
--- scripts/call-recording_transcribe_voicebase_url.rb
+++ scripts/call-recording_transcribe_voicebase_url.rb
@@ -2,6 +2,7 @@
 
 require 'multi_json'
 require 'ringcentral_sdk'
+require 'voicebase'
 require 'pp'
 
 # Set your credentials in the .env file
@@ -12,51 +13,6 @@ config = RingCentralSdk::REST::Config.new.load_dotenv
 client = RingCentralSdk::REST::Client.new
 client.app_config(config.app)
 client.authorize_user(config.user)
-
-module VoiceBase
-  class Client
-    def initialize(api_key, password, transcript_type='machine-best')
-      @api_key = api_key
-      @password = password
-      @transcript_type = transcript_type
-      @conn = Faraday.new(:url => 'https://api.voicebase.com/services') do |faraday|
-        faraday.request  :url_encoded             # multipart/form-data
-        faraday.response :json
-        faraday.response :logger                  # log requests to STDOUT
-        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-      end
-    end
-
-    def upload_media(opts={})
-      params = {
-        :version        => '1.1',
-        :apikey         => @api_key,
-        :password       => @password,
-        :action         => 'uploadMedia',
-        :title          => DateTime.now.strftime('%Y-%m-%d %I:%M:%S %p'),
-        :transcriptType => @transcript_type,
-        :desc           => 'file description',
-        :recordedDate   => DateTime.now.strftime('%Y-%m-%d %I:%M:%S'),
-        :collection     => '',
-        :public         => false,
-        :sourceUrl      => '',
-        :lang           => 'en',
-        :imageUrl       => ''
-      }
-      if opts.has_key?(:filepath) && opts.has_key?(:content_type)
-        params[:file] = Faraday::UploadIO.new(filepath, content_type)
-      elsif opts.has_key?(:mediaUrl)
-        params[:mediaUrl] = opts[:mediaUrl]
-      else
-        raise "Neither file or mediaUrl have been set"
-      end
-      response = @conn.post '/services', params
-      pp response
-      puts response.body['fileUrl']
-      return response
-    end
-  end
-end
 
 def transcribe_recordings(rcsdk, vbsdk)
   # Retrieve voice call log records with recordings
@@ -76,15 +32,15 @@ def transcribe_recordings(rcsdk, vbsdk)
 
       content_uri += '?access_token=' + rcsdk.token.token.to_s
 
-      response_vb = vbsdk.upload_media({:mediaUrl => content_uri.to_s})
+      response_vb = vbsdk.upload_media(:mediaUrl => content_uri.to_s)
 
-      pp(response_vb.body)
+      pp response_vb.body
       
     end
   end
 end
 
-vbsdk = VoiceBase::Client.new(
+vbsdk = VoiceBase::V1::Client.new(
   config.env.data['RC_DEMO_VB_API_KEY'],
   config.env.data['RC_DEMO_VB_PASSWORD'])
 
