bbdce34accfc473cbe0d7988f8f2ed4b52bf8283
John Wang <johncwang@gmail.com>
2016-08-22 09:32:51 -0700

    add sms_subscription.rb

---
+++ scripts/sms_subscription.rb
@@ -0,0 +1,42 @@
+#!ruby
+
+require 'multi_json'
+require 'ringcentral_sdk'
+
+# Set your credentials in the .env file
+# Use the rc_config_sample.env.txt file as a scaffold
+
+config = RingCentralSdk::REST::Config.new.load_dotenv
+
+client = RingCentralSdk::REST::Client.new
+client.set_app_config config.app
+client.authorize_user config.user
+
+# An example observer object
+class MyObserver
+  def update(message)
+    puts "Subscription Message Received"
+    puts JSON.dump(message)
+  end
+end
+
+def run_subscription(client)
+  # Create an observable subscription and add your observer
+  sub = client.create_subscription()
+  sub.subscribe([
+    "/restapi/v1.0/account/~/extension/~/message-store/instant?type=SMS",
+  ])
+
+  sub.add_observer MyObserver.new()
+
+  puts "Click any key to finish"
+
+  stop_script = gets
+
+  # End the subscription
+  sub.destroy()
+end
+
+run_subscription client
+
+puts "DONE"
