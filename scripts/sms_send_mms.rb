6cda516f228d4d13d9f2e7776bbeb3dc136e3a5a
John Wang <johncwang@gmail.com>
2018-04-03 12:29:59 -0700

    update MMS example

--- scripts/sms_send_mms.rb
+++ scripts/sms_send_mms.rb
@@ -1,34 +1,48 @@
 #!ruby
 
+require 'dotenv'
 require 'ringcentral_sdk'
 require 'pp'
 
 # Set your credentials in the .env file
 # Use the rc_config_sample.env.txt file as a scaffold
 
+envPath = ENV['ENV_PATH'] || '.env'
+Dotenv.load envPath
+
 client = RingCentralSdk::REST::Client.new do |config|
-  config.load_env = true
+  config.server_url = ENV['RINGCENTRAL_SERVER_URL']
+  config.app_key    = ENV['RINGCENTRAL_CLIENT_ID']
+  config.app_secret = ENV['RINGCENTRAL_CLIENT_SECRET']
+  config.username   = ENV['RINGCENTRAL_USERNAME']
+  config.extension  = ENV['RINGCENTRAL_EXTENSION']
+  config.password   = ENV['RINGCENTRAL_PASSWORD']
 end
 
 def send_mms_object(client)
-  req = RingCentralSdk::REST::Request::SMS.new.
-    add_metadata({
-      to: ENV['RC_DEMO_SMS_TO'],
-      from: ENV['RC_DEMO_SMS_FROM'],
-      text: ENV['RC_DEMO_SMS_TEXT']
-    }).
-    add_file ENV['RC_DEMO_MMS_FILE']
+  req = RingCentralSdk::REST::Request::Multipart.new(
+    method: 'post',
+    url: '/restapi/v1.0/account/~/extension/~/sms'
+  ).
+  add_json({
+    to:   [{phoneNumber: ENV['RINGCENTRAL_DEMO_SMS_TO']}],
+    from: {phoneNumber: ENV['RINGCENTRAL_DEMO_SMS_FROM']},
+    text: ENV['RINGCENTRAL_DEMO_SMS_TEXT']
+  }).
+  add_file(ENV['RINGCENTRAL_DEMO_MMS_FILE'])
   res = client.send_request req
 end
 
 def send_mms_quick(client)
   client.messages.sms.create(
-    to: [ENV['RC_DEMO_SMS_TO']],
-    from: ENV['RC_DEMO_SMS_FROM'],
-    text: ENV['RC_DEMO_SMS_TEXT'],
-    media: ENV['RC_DEMO_MMS_FILE']
+    to:    [ENV['RINGCENTRAL_DEMO_SMS_TO']],
+    from:  ENV['RINGCENTRAL_DEMO_SMS_FROM'],
+    text:  ENV['RINGCENTRAL_DEMO_SMS_TEXT'],
+    media: ENV['RINGCENTRAL_DEMO_MMS_FILE']
   )
 end
+
+pp({    to:   [ENV['RINGCENTRAL_DEMO_SMS_TO']],    from: ENV['RINGCENTRAL_DEMO_SMS_FROM'],    text: ENV['RINGCENTRAL_DEMO_SMS_TEXT']  })
 
 res = send_mms_object client
 
