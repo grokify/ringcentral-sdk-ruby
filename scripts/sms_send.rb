a340f9de7b78c837f3a36ac71ad9d8b9117a0fda
John Wang <johncwang@gmail.com>
2018-04-06 00:48:33 -0700

    update SMS demo

--- scripts/sms_send.rb
+++ scripts/sms_send.rb
@@ -1,19 +1,29 @@
 #!ruby
 
+require 'dotenv'
 require 'ringcentral_sdk'
 require 'pp'
 
 # Set your credentials in the .env file
 # Use the rc_config_sample.env.txt file as a scaffold
 
+envPath = ENV['ENV_PATH'] || '.env'
+Dotenv.load envPath
+
 client = RingCentralSdk::REST::Client.new do |config|
-  config.load_env = true
+  config.server_url = ENV['RINGCENTRAL_SERVER_URL']
+  config.app_key    = ENV['RINGCENTRAL_CLIENT_ID']
+  config.app_secret = ENV['RINGCENTRAL_CLIENT_SECRET']
+  config.username   = ENV['RINGCENTRAL_USERNAME']
+  config.extension  = ENV['RINGCENTRAL_EXTENSION']
+  config.password   = ENV['RINGCENTRAL_PASSWORD']
 end
 
+
 res = client.messages.sms.create(
-  from: ENV['RC_DEMO_SMS_FROM'],
-  to:   ENV['RC_DEMO_SMS_TO'],
-  text: ENV['RC_DEMO_SMS_TEXT']
+  from: ENV['RINGCENTRAL_DEMO_SMS_FROM'],
+  to:   ENV['RINGCENTRAL_DEMO_SMS_TO'],
+  text: ENV['RINGCENTRAL_DEMO_SMS_TEXT']
 )
 
 pp res.body
