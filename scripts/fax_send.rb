b67b61c26bfa374427f521c61e3b62e6f2d5c8cc
grokify <johncwang@gmail.com>
2018-04-22 14:30:14 -0700

    update scripts/fax_send.rb

--- scripts/fax_send.rb
+++ scripts/fax_send.rb
@@ -1,23 +1,27 @@
 #!ruby
 
+require 'dotenv'
 require 'ringcentral_sdk'
 require 'pp'
 
 # Set your credentials in the .env file
 # Use the rc_config_sample.env.txt file as a scaffold
 
-envPath = ENV['ENV_PATH'] || '.env'
-puts envPath
-Dotenv.load envPath
+Dotenv.load ENV['ENV_PATH'] || '.env'
 
 client = RingCentralSdk::REST::Client.new do |config|
-  config.load_env = true
+  config.server_url = ENV['RINGCENTRAL_SERVER_URL']
+  config.app_key    = ENV['RINGCENTRAL_CLIENT_ID']
+  config.app_secret = ENV['RINGCENTRAL_CLIENT_SECRET']
+  config.username   = ENV['RINGCENTRAL_USERNAME']
+  config.extension  = ENV['RINGCENTRAL_EXTENSION']
+  config.password   = ENV['RINGCENTRAL_PASSWORD']
 end
 
 res = client.messages.fax.create(
-  to:            ENV['RC_DEMO_FAX_TO'],
-  coverPageText: ENV['RC_DEMO_FAX_COVERPAGE_TEXT'],
-  files:        [ENV['RC_DEMO_FAX_FILE']]
+  to:            ENV['RINGCENTRAL_DEMO_FAX_TO'],
+  coverPageText: ENV['RINGCENTRAL_DEMO_FAX_COVERPAGE_TEXT'],
+  files:        [ENV['RINGCENTRAL_DEMO_FAX_FILE']]
 )
 
 pp res.body
